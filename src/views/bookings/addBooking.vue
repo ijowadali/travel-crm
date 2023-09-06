1aac276b8227d5e0ff520a94173fbc785c92a124
jawadali <jawadali20017@gmail.com>
2023-09-06 06:10:26 +0500

    booking fixes and ui changes

--- src/views/bookings/addBooking.vue
+++ src/views/bookings/addBooking.vue
@@ -2,7 +2,7 @@
   <n-card>
     <n-form ref="formRef" :label-width="80" :model="formValue" :rules="rules" size="small">
       <n-space style="display: block" vertical>
-        <n-card title="Booking Details" style="margin-bottom: 10px">
+        <n-card style="margin-bottom: 10px" title="Booking Details">
           <n-row gutter="12">
             <n-col :span="6">
               <n-form-item label="Customer Name" path="customer_name">
@@ -16,19 +16,19 @@
               <n-form-item label="Booking Status" path="booking_status">
                 <n-select
                   v-model:value="formValue.booking_status"
-                  filterable
-                  placeholder="Search Status"
                   :options="[
                     { label: 'Draft', value: 'draft' },
                     { label: 'Processing', value: 'processing' },
                     { label: 'Final', value: 'Final' },
                   ]"
+                  filterable
+                  placeholder="Search Status"
                 />
               </n-form-item>
             </n-col>
             <n-col :span="6">
               <n-form-item label="Group No" path="group_no">
-                <n-input-number class="w-full" v-model:value="formValue.group_no" min="0" />
+                <n-input-number v-model:value="formValue.group_no" class="w-full" min="0" />
               </n-form-item>
             </n-col>
             <n-col :span="6">
@@ -40,36 +40,36 @@
               <n-form-item label="Category" path="category">
                 <n-select
                   v-model:value="formValue.category"
-                  filterable
-                  placeholder="Search Category"
                   :options="[
                     { label: 'Basic', value: 'basic' },
                     { label: 'Individual', value: 'individual' },
                     { label: 'Premium', value: 'premium' },
                     { label: 'VIP', value: 'vip' },
                   ]"
+                  filterable
+                  placeholder="Search Category"
                 />
               </n-form-item>
             </n-col>
             <n-col :span="6">
               <n-form-item label="Approval Date" path="approval_date">
                 <n-date-picker
-                  style="width: 100%"
                   v-model:formatted-value="formValue.approval_date"
-                  value-format="yyyy-MM-dd HH:mm:ss"
-                  type="datetime"
                   clearable
+                  style="width: 100%"
+                  type="datetime"
+                  value-format="yyyy-MM-dd HH:mm:ss"
                 />
               </n-form-item>
             </n-col>
             <n-col :span="6">
               <n-form-item label="Expected Departure" path="expected_departure">
                 <n-date-picker
-                  style="width: 100%"
                   v-model:formatted-value="formValue.expected_departure"
-                  value-format="yyyy-MM-dd HH:mm:ss"
-                  type="datetime"
                   clearable
+                  style="width: 100%"
+                  type="datetime"
+                  value-format="yyyy-MM-dd HH:mm:ss"
                 />
               </n-form-item>
             </n-col>
@@ -105,7 +105,10 @@
 
   async function saveBooking() {
     formRef.value?.validate(async (errors) => {
-      if (!errors) {
+      if (errors) {
+        console.log(errors);
+        window['$message'].error('Please fill out required fields');
+      } else {
         loading.value = true;
         const response = await createBookingApi({ ...formValue.value });
         if (response.status) {
@@ -114,9 +117,6 @@
           window['$message'].error(response.message);
         }
         loading.value = false;
-      } else {
-        console.log(errors);
-        window['$message'].error('Please fill out required fields');
       }
     });
   }
