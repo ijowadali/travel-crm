7c1abf7ca31f9ae5c6136b873c149fb398793414
jawad <jawadali20017@gmail.com>
2023-09-30 10:13:06 +0500

    Booking Changes some other changes

--- src/components/permission/AssignPermissions.vue
+++ src/components/permission/AssignPermissions.vue
@@ -4,24 +4,28 @@
       <NCard embedded class="mb-2 px-3 py-2" :content-style="{ padding: 0 }">
         <div class="flex flex-col items-center sm:flex-row sm:justify-between">
           <h3 class="text-lg">Permissions List</h3>
-          <NButton secondary type="info" size="medium" @click="handleAssignPermissions">
+          <NButton secondary type="info" size="small" @click="handleAssignPermissions">
             Assign Permission
           </NButton>
         </div>
       </NCard>
     </template>
     <n-tabs
-      type="card"
-      size="medium"
-      default-value="Dashboard"
-      pane-class="border-x border-b overflow-scroll h-full"
-      class="sticky top-0 h-full overflow-scroll"
+        type="card"
+        size="medium"
+        default-value="Dashboard"
+        pane-class="border-x border-b overflow-scroll h-full"
+        class="sticky top-0 h-full overflow-scroll"
     >
-      <n-tab-pane v-for="item of menus" :key="item.id" :name="item.menu_name" :tab="item.menu_name">
+      <n-tab-pane v-for="item of menus" :key="item.id" :name="item.menu_name"
+                  :tab="item.menu_name">
         <n-checkbox-group class="mx-2" v-model:value="selectedPermissions">
           <n-row>
             <n-col v-for="permission of item.permissions" :key="permission.id" :span="6">
-              <n-checkbox :value="permission.id" :label="permission.name" />
+              <n-checkbox :value="permission.id" :label="permission.name"/>
+              <n-tag :bordered="false" :type="permission.type === 'private' ? 'error' : 'success'">
+                {{ permission.type }}
+              </n-tag>
             </n-col>
           </n-row>
         </n-checkbox-group>
@@ -30,13 +34,13 @@
   </ContentLayout>
 </template>
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
-import { usefilterMenu } from '@src/filters/menus';
+import {onMounted, ref} from 'vue';
+import {useRoute, useRouter} from 'vue-router';
+import {usefilterMenu} from '@src/filters/menus';
 import ContentLayout from '@src/layouts/ContentLayout/index.vue';
-import { getRecordApi, updateRecordApi } from '@src/api/endpoints';
+import {getRecordApi, updateRecordApi} from '@src/api/endpoints';
 
-const { menus, getMenus } = usefilterMenu();
+const {menus, getMenus} = usefilterMenu();
 const route = useRoute();
 const router = useRouter();
 const userData = ref({});
@@ -62,7 +66,7 @@ onMounted(() => {
       window['$message'].success(res.message);
     });
   } else {
-    router.replace({ name: 'ErrorPageSon' });
+    router.replace({name: 'ErrorPageSon'});
   }
 });
 
